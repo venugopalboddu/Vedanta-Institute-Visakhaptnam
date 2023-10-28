d085725c4b0cc9d2ec2c1aadd6929dd1796ef830
Venugopal Boddu <venu@fx-pros.com>
2023-10-28 09:54:29 +0530

    new-updated-changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  countDownDate = new Date("dec 7, 2023 18:30:00").getTime();
+  days:any
+  hours:any
+  minutes:any
+  seconds:any
+  timeup:any
   constructor() { 
     window.scrollTo(0, 0);
   }
 
   ngOnInit() {
-  }
+  } 
+  
+  x = setInterval(()=>{
+    var now = new Date().getTime();
+    var distance = this.countDownDate - now;
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((distance%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    if(distance < 0){
+      clearInterval(this.x)
+      this.timeup = 'Event Closed'
+      this.days = 0
+      this.hours = 0
+      this.minutes = 0
+      this.seconds = 0
+    }
+  })
+
+ 
 }
